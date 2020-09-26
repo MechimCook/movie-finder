f81342baddee5dd9fdd89f2fc66c0b941965c2a7
Mechim <mechim.cook20@gmail.com>
2020-09-26 15:12:17 -0400

    Require input

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Form = props => (
 	<form action="/movies/">
-		<input type="text" name="query" placeholder="movie name"/>
+		<input type="text" name="query" placeholder="movie name" required/>
 		<input type="hidden" id="page" name="page" value="1"/>
 		<button>Get movie</button>
 	</form>
