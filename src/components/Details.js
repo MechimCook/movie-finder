28a61b3d7fcc286a8ad7bc4f66c471fb1d6a2871
Mechim <mechim.cook20@gmail.com>
2020-09-28 15:37:41 -0400

    Account for empty results

--- src/components/Details.js
+++ src/components/Details.js
@@ -23,38 +23,63 @@ class Results extends React.Component {
   render() {
 
 const data = this.state.data
-let thing = ""
+let details = ""
     if (data) {
-      const genres = data.genres.map(genre => genre.name).join(', ');
       const title = data.title;
       const tagline = data.tagline
       const rating = data.vote_average
       const votes = data.vote_count
       const description = data.overview
-      const producedBy = data.production_companies.map(company => company.name).join(', ');
-      const producedIn = data.production_countries.map(country => country.name).join(', ');
-      const releaseDate = new Date(data.release_date);
-      const runTime = `${Math.floor(data.runtime / 60)}:${data.runtime % 60}`;
-      const languages = data.spoken_languages.map(language => language.name).join(', ');
+
+      const genres = data.genres.length > 0 ?
+      <div>Genres: {data.genres.map(genre => genre.name).join(', ')}</div> : null;
+
+      const producedBy = data.production_companies.length > 0 ?
+      <div>Produced By: {data.production_companies.map(company => company.name).join(', ')}</div> : null;
+
+      const producedIn = data.production_countries.length > 0 ?
+      <div> Produced In: {data.production_countries.map(country => country.name).join(', ')}</div> : null;
+
+      const languages = data.spoken_languages.length > 0 ?
+       <div >Languages Spoken: {data.spoken_languages.map(language => language.name).join(', ')}</div> : null;
+
+      const releaseDate = data.release_date ?
+      <h4>Release Date: {new Date(data.release_date).toDateString()}</h4> : null;
+
+      const runTime = data.runtime ?
+      <h4>Run Time: {`${Math.floor(data.runtime / 60)}:${data.runtime % 60}`}</h4> : null;
+
       const homePage = data.homepage === "" ? null :
           <button onClick={event => window.location.href=data.homepage}>Watch This Movie</button>
 
       const castAndCrew = data.credits
-      const cast = castAndCrew.cast.map(actor =>
+
+      const cast = castAndCrew.cast.length > 0 ?
+      <div>
+      <h4>Cast</h4>
+      {castAndCrew.cast.map(actor =>
         <div className="actor-card" key= {actor.name}>
           <img className="icon" src={`https://image.tmdb.org/t/p/original${actor.profile_path}`} onError={(e)=>{e.target.onerror = null; e.target.src=defaultImage}} alt="icon" />
             <div className="card-body">
               <h5 >{actor.name} as {actor.character}</h5>
             </div>
-        </div>);
-        const crew = castAndCrew.crew.map(member =>
+        </div>)}
+      </div> : null ;
+
+
+        const crew = castAndCrew.crew.length > 0 ?
+        <div>
+        <h4>Crew</h4>
+        {castAndCrew.crew.map(member =>
           <div className="actor-card" key= {member.name}>
             <img className="icon" src={`https://image.tmdb.org/t/p/original${member.profile_path}`} onError={(e)=>{e.target.onerror = null; e.target.src=defaultImage}} alt="icon" />
               <div className="card-body">
                 <h5 >{member.job}: {member.name}</h5>
               </div>
-          </div>);
-    thing =
+          </div>)}
+        </div> : null;
+
+    details =
     <div className="movie__detail">
     <h3>{title}</h3>
     <h4>{tagline}</h4>
@@ -63,32 +88,20 @@ let thing = ""
     <img className="detail-img" src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`} onError={(e)=>{e.target.onerror = null;}} alt=""/>
 
     <p className="detail-desc">{description}</p>
-
-      <div>Genres: {genres}</div>
-
-      <div>Produced By: {producedBy}</div>
-
-      <div> Produced In: {producedIn}</div>
-
-      <div >Languages Spoken: {languages}</div>
-
-      <h4>Release Date: {releaseDate.toDateString()}</h4>
-      <h4>Run Time: {runTime}</h4>
-
-      {homePage}
-    <div>
-    <h4>Cast</h4>
+    {genres}
+    {producedBy}
+    {producedIn}
+    {languages}
+    {releaseDate}
+    {runTime}
+    {homePage}
     {cast}
-    </div>
-    <div>
-    <h4>Crew</h4>
     {crew}
-    </div>
     </div>
   }
 	return (
     <div className="movie__info">
-    {thing}
+    {details}
      </div>
 	)
 }
